f6e39a08b015e8ea9baeb47e2f0c21502693e3d8
ashiqxq <ashiqxq@gmail.com>
2021-07-27 10:47:26 +0530

    broadcast-events-157

--- src/index.js
+++ src/index.js
@@ -12,13 +12,23 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 app.use(express.static(publicDirectoryPath))
 
 
-let message = 'welcome1'
+let welcome_message = 'welcome1'
+let new_user_message = 'New user has joined'
+let user_left_message = 'An user has left the chat'
 io.on('connection', (socket)=>{
-    console.log("New websocket connection work")
+    console.log("New websocket connection work");
+    socket.emit('message', welcome_message);
+    socket.broadcast.emit('message', new_user_message);
+
     socket.on('sendMessage', (message)=>{
         io.emit('message', message);
     })
+    socket.on('disconnect', ()=>{
+        io.emit('message', user_left_message);
+    })
 })
+
+
 server.listen(port, ()=>{
     console.log(`Server is up on port ${port}!`);
 });
