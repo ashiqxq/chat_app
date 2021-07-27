841307beb2f11406d7281d6fdc7fe6de0edccaa7
ashiqxq <ashiqxq@gmail.com>
2021-07-27 14:14:38 +0530

    working-with-time-163

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const socketio = require('socket.io')
 const Filter = require('bad-words');
 const app = express()
+const {generateMessage} = require('./utils/messages')
 const server = http.createServer(app)
 const io = socketio(server)
 
@@ -18,23 +19,23 @@ let new_user_message = 'New user has joined'
 let user_left_message = 'An user has left the chat'
 io.on('connection', (socket)=>{
     console.log("New websocket connection work");
-    socket.emit('message', welcome_message);
-    socket.broadcast.emit('message', new_user_message);
+    socket.emit('message', generateMessage(welcome_message));
+    socket.broadcast.emit('message', generateMessage(new_user_message));
 
     socket.on('sendMessage', (message, callback)=>{
         const filter = new Filter();
         if (filter.isProfane(message)){
             return callback('Profanity detected')
         }
-        io.emit('message', message);
+        io.emit('message', generateMessage(message));
         callback('Delivered');
     })
     socket.on('sendLocation', (message, callback)=>{
-        socket.broadcast.emit('locationMessage', message);
+        socket.broadcast.emit('locationMessage', generateMessage(message));
         callback();
     })
     socket.on('disconnect', ()=>{
-        io.emit('message', user_left_message);
+        io.emit('message', generateMessage(user_left_message));
     })
 })
 
