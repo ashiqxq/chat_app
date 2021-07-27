80083e58e92026f34e399a6eece5e2411effb387
ashiqxq <ashiqxq@gmail.com>
2021-07-27 15:37:38 +0530

    socket-rooms-167

--- src/index.js
+++ src/index.js
@@ -15,19 +15,22 @@ app.use(express.static(publicDirectoryPath))
 
 
 let welcome_message = 'welcome1'
-let new_user_message = 'New user has joined'
+let new_user_message = ' has joined'
 let user_left_message = 'An user has left the chat'
 io.on('connection', (socket)=>{
     console.log("New websocket connection work");
-    socket.emit('message', generateMessage(welcome_message));
-    socket.broadcast.emit('message', generateMessage(new_user_message));
 
+    socket.on('join', ({username, room})=>{
+        socket.join(room)
+        socket.emit('message', generateMessage(welcome_message));
+        socket.broadcast.to(room).emit('message', generateMessage(username+new_user_message));
+    })
     socket.on('sendMessage', (message, callback)=>{
         const filter = new Filter();
         if (filter.isProfane(message)){
             return callback('Profanity detected')
         }
-        io.emit('message', generateMessage(message));
+        io.to('thunder').emit('message', generateMessage(message));
         callback('Delivered');
     })
     socket.on('sendLocation', (message, callback)=>{
