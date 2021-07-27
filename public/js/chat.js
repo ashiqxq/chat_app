f31e3f9644de46e909be9d70cec88c96e9b1bbda
ashiqxq <ashiqxq@gmail.com>
2021-07-27 12:14:25 +0530

    form-and-button-states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,23 @@
 const socket = io()
 
+//Elements
+const $messageForm = document.querySelector('#message-form')
+const $messageFormInput  = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.querySelector('#send-location')
 socket.on('message', (message)=>{
     console.log(message);
 })
 
-document.querySelector('#message-form')
-    .addEventListener('submit', 
+$messageForm.addEventListener('submit', 
         (e)=>{
             e.preventDefault();
+            $messageFormButton.setAttribute('disabled', 'disabled')
+            $messageFormInput.value = ''
+            $messageFormInput.focus()
             const message = e.target.elements.message.value;
             socket.emit('sendMessage', message, (error)=>{
+                $messageFormButton.removeAttribute('disabled')
                 if (error){
                     return console.log(error)
                 }
@@ -17,14 +25,16 @@ document.querySelector('#message-form')
             });
 })
 
-document.querySelector('#send-location').addEventListener(
+$sendLocationButton.addEventListener(
             'click', ()=>{
                 if (!navigator.geolocation){
                     return alert('geolocation is not supported by your browser');
                 }
+                $sendLocationButton.setAttribute('disabled', 'disabled')
                 current_location = navigator.geolocation.getCurrentPosition((locData)=>{
                      socket.emit('sendLocation', `https://google.com/maps?q=${locData.coords.latitude},${locData.coords.longitude}`, 
                      ()=>{
+                         $sendLocationButton.removeAttribute('disabled')
                         console.log("location shared!")
                      });
                     // return locData.coords;
