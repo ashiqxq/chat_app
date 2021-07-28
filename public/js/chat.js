8dd744624e30d6712eb606fb1f1150e73d53b8e9
ashiqxq <ashiqxq@gmail.com>
2021-07-28 10:45:00 +0530

    automatic-scrolling-173

--- public/js/chat.js
+++ public/js/chat.js
@@ -14,6 +14,26 @@ const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 //Options
 const {username, room} = Qs.parse(location.search, {ignoreQueryPrefix: true })
 
+const autoscroll = () =>{
+    // new message element
+    const $newMessage = $messages.lastElementChild;
+    // height of new message
+    const newMessageStyles = getComputedStyle($newMessage)
+    const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+    const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+    // visible height
+    const visibleHeight = $messages.offsetHeight;
+
+    //height of messages container
+    const containerHeight = $messages.scrollHeight
+    //how far have i scrolled
+    const scrollOffset = $messages.scrollTop + visibleHeight
+
+    if (containerHeight-newMessageHeight<=scrollOffset){
+        $messages.scrollTop = $messages.scrollHeight;
+    }
+}
+
 socket.on('message', (message)=>{
     console.log(message);
     const html = Mustache.render(messageTemplate, {
@@ -22,6 +42,7 @@ socket.on('message', (message)=>{
         createdAt: moment(message.createdAt).format('h:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
 })
 
 socket.on('locationMessage', (message)=>{
@@ -33,6 +54,7 @@ socket.on('locationMessage', (message)=>{
 
     })
     $messages.insertAdjacentHTML('beforeend', html)
+    autoscroll()
 })
 
 socket.on('roomData', ({room, users}) => {
